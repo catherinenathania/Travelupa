d1464ccbda10039b5271bb8b9dfb61d9e48589e7
catherinenathania <149342934+catherinenathania@users.noreply.github.com>
2025-12-16 14:01:01 +0700

    Delete BAB 3 (Jetpack Compose Bagian 2) directory

--- BAB 3 (Jetpack Compose Bagian 2)/File/Wisata.kt
+++
@@ -1,9 +0,0 @@
-package com.example.travelupa.model
-
-data class Wisata(
-    val id: Int,
-    val nama: String,
-    val lokasi: String, // Contoh: Malang, Banyuwangi
-    val deskripsiSingkat: String,
-    val gambarResId: Int // Resource ID gambar (dari R.drawable)
-)
